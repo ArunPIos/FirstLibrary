820fcc99f7e8fd1aebfcc71d95ac58e70d5af0e5
Arun_Pandian <arun_pandian@Arun-MacBook-Air.local>
2024-03-05 15:07:46 +0530

    Added all Files of Library here

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+
+public struct CustomSDK{
+    public static func addNum(num1 : Int = 0, num2: Int = 0) -> Int{
+        num1 + num2
+    }
+    
+    public static func printStatment(){
+        print("Statement")
+    }
+}

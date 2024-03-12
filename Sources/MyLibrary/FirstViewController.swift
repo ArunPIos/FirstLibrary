2bae381ed73779253635de2424886b20ed92ace6
ArunPIos <arunpandian2041@gmail.com>
2024-03-12 13:08:31 +0530

    adding my first ui in this package

---
+++ Sources/MyLibrary/FirstViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.swift
+//  
+//
+//  Created by Arun_Pandian on 12/03/24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var msgLbl: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tabBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+        self.tabBtn.tintColor = .white
+        self.tabBtn.backgroundColor = .systemBlue
+       
+    }
+    
+
+    @IBAction func tapAction(_ sender: Any) {
+        guard let textField = textField.text else {return}
+        msgLbl.text = textField
+    }
+    
+
+}
--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -2,14 +2,23 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+import UIKit
 
 
 public struct CustomSDK{
+    var window : UIWindow?
     public static func addNum(num1 : Int = 0, num2: Int = 0) -> Int{
         num1 + num2
     }
     
     public static func printStatment(){
         print("Statement")
+    }
+    
+    public static func getFirstScreeen(window: UIWindow){
+        let storyBoard = UIStoryboard(name: "Storyboard", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "FirstViewController")
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
     }
 }
